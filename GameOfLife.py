975b0f0e4af876d27af3fd88ae7164a1b5608006
mjkaufer <mjkaufer@gmail.com>
2015-04-27 18:53:19 -0400

    Buggy functionality

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,29 +1,160 @@
 import sublime
 import sublime_plugin
+import random
+
+class AnimateCommand(sublime_plugin.TextCommand):
+    def run(self, edit, running):
+        # if not running:
+        #     return
+
+        tabSize = self.view.settings().get("tab_size")
+
+        # print(running)
+        # applies animation
+        totalRegion = sublime.Region(0, self.view.size())
+        charGrid = []
+        lines = self.view.lines(totalRegion)
+        for r in range(len(lines)):
+            line = lines[r]
+
+            charGrid.append([])
+
+            for c in range(line.begin(), line.end()):
+                char = sublime.Region(c, c+1)
+                c = c - line.begin()
+                if self.view.substr(char) == "\t": # we don't want tabs, but rather spaces
+                    charGrid[r] += [" "] * tabSize
+                else:
+                    charGrid[r].append(self.view.substr(char))
+
+        newGrid = self.nextFrame(charGrid)
+
+        newContents = ""
+
+        for r in range(len(newGrid)):
+            for c in range(len(newGrid[r])):
+                newContents += newGrid[r][c]
+            newContents += "\n"
+
+        newContents = newContents[:len(newContents) - 1] # to get rid of trailing newline
+
+        self.view.replace(edit, totalRegion, newContents)
+
+        # print(charGrid)
+
+        
+        
+
+    def getNeighbors(self, grid, row, col):
+
+        neighbors = []
+
+        for r in range(-1, 1+1):
+            for c in range(-1, 1+1):
+                if r == c == 0:
+                    continue
+                nr = row + r
+                nc = col + c
+
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[nr]): # if this position is defined
+                    cell = grid[nr][nc]
+                    if living(cell):
+                        neighbors.append(cell)
+
+        return neighbors
+
+    def nextFrame(self, grid):
+
+        newGrid = grid[:][:]
+
+        for r in range(len(newGrid)):
+            for c in range(len(newGrid[r])):
+                cell = grid[r][c]
+                neighbors = self.getNeighbors(grid, r, c)
+                if living(cell):
+                    if 2 <= len(neighbors) <= 3:
+                        # cell survives
+                        continue
+                    else:
+                        newGrid[r][c] = " "
+                else:
+                    if len(neighbors) == 3:
+                        newGrid[r][c] = generateLivingCharacter(neighbors)
+
+        return newGrid
+
+
+
+
+
 
 class SimulateCommand(sublime_plugin.TextCommand):
 
     running = False
+    fileBuffer = ""
 
     def run(self, edit):
 
-        if running:
+        print("Before", self.running)
+        if self.running:
+            self.running = not self.running
+            self.stopSimulation(edit)
             # the game of life is already running - the user wants to revert the game
             # stop game of life, revert to original file contents
-
+            
         else:
+            self.running = not self.running
+            self.startSimulation(edit)
             # back up file contents to revert to later
             # run game of life at interval of x seconds
             # parts which match the \w regex are deemed living, everything else is considered dead
 
-        running = not running
+        print("After", self.running)
 
-    def living(char):
-        return ord('a') <= ord(char.lower()) <= ord('z')
 
-    def generateLivingCharacter(livingCharacters):
-        # generates a living character based on the ord value of the letters, along with the capitalization
 
-        avg = int(sum([ord(l.lower()) for l in livingCharacters]) / len(livingCharacters))
-        uppercaseProbability = sum([l.upper() == l for l in livingCharacters]) / len(livingCharacters)
+    def startSimulation(self, edit):
+        fileRegion = sublime.Region(0, self.view.size())
+
+        self.fileBuffer = self.view.substr(fileRegion)
+
+        self.animate(0)
+
+    def stopSimulation(self, edit):
+        self.view.replace(edit, sublime.Region(0, self.view.size()), self.fileBuffer)
+
+    def animate(self, x):
+        print("A",self.running)
+        if not self.running:
+            return
+        self.view.run_command("animate", {"running": self.running})
+        
+        # if running:
+        sublime.set_timeout_async(lambda: self.animate(x+1), 100)
+
+
+
+
+
+
+
+def living(char):
+    return ord('a') <= ord(char.lower()) <= ord('z')
+
+def generateLivingCharacter(livingCharacters):
+    # generates a living character based on the ord value of the letters, along with the capitalization
+
+    avg = int(sum([ord(l.lower()) for l in livingCharacters]) / len(livingCharacters)) # letter's char code
+
+    char = chr(avg)
+
+    uppercaseProbability = sum([l.upper() == l for l in livingCharacters]) / len(livingCharacters)
+
+    if random.random() < uppercaseProbability:
+        char = char.upper()
+
+    return char
+
+def p():
+    print("AAA")
         
